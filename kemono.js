98bd942a5a3cd9cdce3827265e31d048f38c6111
october10th <venitdubitare@gmail.com>
2018-01-22 10:02:26 -0500

    add pic

--- kemono.js
+++ kemono.js
@@ -60,7 +60,8 @@ function replaceImages(selector, node) {
         'C8VfhXbUMAQbOgs.jpg',  // snow
         'DTvfza4VAAE8u8h.jpg',  // scramble
         'DTvf2MeUQAEoVlE.jpg',  // ball
-        'DT_3DC1VQAE1Cth.jpg'   // ボブネミミッミ
+        'DT_3DC1VQAE1Cth.jpg',  // ボブネミミッミ
+        'DUHBJRJVAAEWvZs.jpg'   // 6
 
         
     ];
